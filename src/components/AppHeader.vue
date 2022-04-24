99a34ee745d35cf200d4b96e7fa8544ef89da164
Furkangunduz <frkn.gundz00@gmail.com>
2022-04-24 18:48:04 +0300

    sidebar olusturuldu.

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,3 +1,27 @@
 <template>
-    <div class="card">asasdsad</div>
+    <header class="bg-gray-100">
+        <div class="flex justify-between items-center py-3 px-2">
+            <div class="flex ml-3">
+                <a href="#"> Home </a>
+            </div>
+            <div>
+                <input
+                    class="bg-slate-300 rounded px-4 py-2 font-light"
+                    type="text"
+                    placeholder="search"
+                />
+            </div>
+            <div class="flex">
+                <div class="dropdowncontainer">
+                    <button class="">Daha Fazla</button>
+                    <nav tabindex="1">
+                        <ul>
+                            <li><a href="#">My Profile</a></li>
+                            <li><a href="#">Logout</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
 </template>
---
+++ src/components/AppSidebar.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="sidebar">
+        <button>Bildirimler</button>
+        <button>Mesajlar</button>
+        <button>Yeni Tivit !</button>
+    </div>
+</template>
